063a79e9e608669bdbb916b4b8784c6abd2dc64d
benhur07b <benhur07b@users.noreply.github.com>
2017-04-04 12:10:44 +0800

    merged api, documents and added api in geonode/urls.py

--- geonode/api/urls.py
+++ geonode/api/urls.py
@@ -3,9 +3,15 @@ from tastypie.api import Api
 from .api import TagResource, TopicCategoryResource, ProfileResource, \
     GroupResource, RegionResource, OwnersResource
 from .resourcebase_api import LayerResource, MapResource, DocumentResource, \
-    ResourceBaseResource, FeaturedResourceBaseResource
+    ResourceBaseResource, FeaturedResourceBaseResource, DownloadCountResource
+from django.conf.urls import patterns, url
 
 api = Api(api_name='api')
+
+urlpatterns = patterns(
+    'geonode.api.views',
+    url(r'^total_count', 'total_count', name="total_count"),
+)
 
 api.register(LayerResource())
 api.register(MapResource())
@@ -18,3 +24,4 @@ api.register(TopicCategoryResource())
 api.register(GroupResource())
 api.register(FeaturedResourceBaseResource())
 api.register(OwnersResource())
+api.register(DownloadCountResource())
