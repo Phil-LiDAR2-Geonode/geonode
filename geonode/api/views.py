efa284c50dc180af41461111dcdb1f0abdacc173
Parmap <you@example.com>
2017-04-04 14:19:52 +0800

    merged api, layers/views.py, layers/urls.py, documents/views.py, and reports for download tracking made changes to layer_detail.html, layer_list.html, geonode/urls.py, and geonode/settings.py

---
+++ geonode/api/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import (
+    redirect, get_object_or_404, render, render_to_response)
+from django.http import HttpResponse, HttpResponseRedirect
+
+import urllib2
+from django.core.urlresolvers import resolve
+import json
+from urlparse import urljoin
+from urllib2 import HTTPError
+from geonode.layers.models import Layer
+
+def total_count(request):
+    output = {}
+    output['total_count'] = Layer.objects.all().count()
+    return HttpResponse(json.dumps(output),mimetype='application/json',status=200)
