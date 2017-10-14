a588bb23688f35670a77f5560c9e1ebc24b7f702
Jok Laurente <jmelaurente@gmail.com>
2017-10-14 21:59:51 +0800

    initial views

--- news/views.py
+++ news/views.py
@@ -4,20 +4,7 @@ from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import render_to_response
 
-def index(request):
+def article_list(request):
     latest_article_list = Article.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('news/index.html',
+    return render_to_response('news/article_list.html',
         RequestContext(request, {'latest_article_list': latest_article_list}))
-
-def detail(request, article_id):
-    try:
-        p = Article.objects.get(pk=article_id)
-    except Article.DoesNotExist:
-        raise Http404
-    return render_to_response('news/detail.html', RequestContext(request, {'article': p}))
-
-def results(request, article_id):
-    return HttpResponse("You're looking at the results of article %s." % article_id)
-
-def vote(request, article_id):
-    return HttpResponse("You're voting on article %s." % article_id)
