04d886252f605060c5b6bb704e7767aace531f32
Jok Laurente <jmelaurente@gmail.com>
2017-10-15 06:11:20 +0800

    added headline in urls

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('news.views',
                         url(r'^$', 'article_list', name='article_list'),
-                        url(r'^(?P<article_id>\d+)/?$', 'article_detail', name='article_detail'),
+                        url(r'^article/(?P<article_id>\d+)/?$', 'article_detail', name='article_detail'),
+                        url(r'^headline/(?P<headline_id>\d+)/?$', 'headline_detail', name='headline_detail'),
                        )
