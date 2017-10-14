036d4e72080c6093db00b68ddcc3c57810ef1900
Jok Laurente <jmelaurente@gmail.com>
2017-10-15 00:34:24 +0800

    updated news.views

--- news/views.py
+++ news/views.py
@@ -8,3 +8,10 @@ def article_list(request):
     latest_article_list = Article.objects.all().order_by('-pub_date')[:5]
     return render_to_response('news/article_list.html',
         RequestContext(request, {'latest_article_list': latest_article_list}))
+
+def article_detail(request, article_id):
+    try:
+        a = Article.objects.get(pk=article_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render_to_response('news/article_detail.html', RequestContext(request, {'article': a}))
