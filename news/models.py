6fc735d36eafa81f79c3afc5b658d45669dfc94e
Jok Laurente <jmelaurente@gmail.com>
2017-10-17 06:26:44 +0800

    removed author and added creation_date

--- news/models.py
+++ news/models.py
@@ -1,21 +1,22 @@
 from django.db import models
+from datetime import datetime
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     image1 = models.FileField('image 1', upload_to="article_img/")
     image2 = models.FileField('image 2', upload_to="article_img/")
+    creation_date = models.DateTimeField(default=datetime.now, editable=False)
     def __unicode__(self):
         return self.title
 
 class Headline(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     image1 = models.FileField('image 1', upload_to="article_img/")
     image2 = models.FileField('image 2', upload_to="article_img/")
+    creation_date = models.DateTimeField(default=datetime.now, editable=False)
     def __unicode__(self):
         return self.title
