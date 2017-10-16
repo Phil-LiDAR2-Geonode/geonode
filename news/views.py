8fd51eccd4bd14d61218309c4c0b2d64c0e64a79
Jok Laurente <jmelaurente@gmail.com>
2017-10-16 23:53:54 +0800

    added month and year list

--- news/views.py
+++ news/views.py
@@ -8,27 +8,43 @@ from django.shortcuts import render_to_response
 def article_list(request):
     latest_article_list = Article.objects.all().order_by('-pub_date')[:5]
     latest_headline = Headline.objects.all().order_by('-pub_date')[0]
-    return render_to_response('news/article_list.html',
-        RequestContext(request, {'latest_article_list': latest_article_list}),
-        RequestContext(request, {'latest_headline': latest_headline}))
+    year_list = Article.objects.dates('pub_date','year', order='DESC')
+    month_list = Article.objects.dates('pub_date','month',order='DESC')
+    return render_to_response('news/article_list.html', RequestContext(request, {
+        'latest_article_list': latest_article_list,
+        'year_list': year_list,
+        'month_list' : month_list,
+        'latest_headline': latest_headline
+        }))
 
 def article_detail(request, article_id):
-    try:
-        a = Article.objects.get(pk=article_id)
-    except Article.DoesNotExist:
-        raise Http404
-    return render_to_response('news/article_detail.html', RequestContext(request, {'article': a}))
+    article = Article.objects.get(pk=article_id)
+    year_list = Article.objects.dates('pub_date','year', order='DESC')
+    month_list = Article.objects.dates('pub_date','month',order='DESC')
+    return render_to_response('news/article_detail.html', RequestContext(request, {
+        'year_list': year_list,
+        'month_list' : month_list,
+        'article': article
+        }))
 
 def headline_detail(request, headline_id):
-    try:
-        h = Headline.objects.get(pk=headline_id)
-    except Headline.DoesNotExist:
-        raise Http404
-    return render_to_response('news/headline_detail.html', RequestContext(request, {'headline': h}))
+    headline = Headline.objects.get(pk=headline_id)
+    year_list = Article.objects.dates('pub_date','year', order='DESC')
+    month_list = Article.objects.dates('pub_date','month',order='DESC')
+    return render_to_response('news/headline_detail.html', RequestContext(request, {
+        'year_list': year_list,
+        'month_list' : month_list,
+        'headline': headline
+        }))
 
 def article_list_filter(request, pub_year, pub_month):
     latest_article_list = Article.objects.filter(pub_date__year=pub_year, pub_date__month=pub_month)
     latest_headline = Headline.objects.all().order_by('-pub_date')[0]
-    return render_to_response('news/article_list.html',
-        RequestContext(request, {'latest_article_list': latest_article_list}),
-        RequestContext(request, {'latest_headline': latest_headline}))
+    year_list = Article.objects.dates('pub_date','year', order='DESC')
+    month_list = Article.objects.dates('pub_date','month',order='DESC')
+    return render_to_response('news/article_list.html', RequestContext(request, {
+        'latest_article_list': latest_article_list,
+        'year_list': year_list,
+        'month_list' : month_list,
+        'latest_headline': latest_headline
+        }))
