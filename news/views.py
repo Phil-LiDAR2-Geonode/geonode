b2589136bc9d2edf251e1d2cf70cbfe73ce0bbc0
Jok Laurente <jmelaurente@gmail.com>
2017-10-17 06:27:45 +0800

    changed pub_date to creation_date in getting latest headline

--- news/views.py
+++ news/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render_to_response
 
 def article_list(request):
     latest_article_list = Article.objects.all().order_by('-pub_date')[:5]
-    latest_headline = Headline.objects.all().order_by('-pub_date')[0]
+    latest_headline = Headline.objects.all().order_by('-creation_date')[0]
     year_list = Article.objects.dates('pub_date','year', order='DESC')
     month_list = Article.objects.dates('pub_date','month',order='DESC')
     return render_to_response('news/article_list.html', RequestContext(request, {
@@ -39,7 +39,7 @@ def headline_detail(request, headline_id):
 
 def article_list_filter(request, pub_year, pub_month):
     latest_article_list = Article.objects.filter(pub_date__year=pub_year, pub_date__month=pub_month)
-    latest_headline = Headline.objects.all().order_by('-pub_date')[0]
+    latest_headline = Headline.objects.all().order_by('-creation_date')[0]
     year_list = Article.objects.dates('pub_date','year', order='DESC')
     month_list = Article.objects.dates('pub_date','month',order='DESC')
     return render_to_response('news/article_list.html', RequestContext(request, {
