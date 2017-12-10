c942dc3b9a3e215c97af8727c3045f1f96f3845e
lionellbriones <llenoil@gmail.com>
2017-12-10 20:00:28 +0800

    Added rs filter and descriptions

--- parmap/views.py
+++ parmap/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render_to_response
 
 # Create your views here.
 def other_rs(request):
-    return render_to_response('parmap/other_rs.html', RequestContext(request, {}))
+    context_dict = {
+        "map_type": request.GET['keywords__slug__in']
+    }
+    
+    return render_to_response('parmap/other_rs.html', RequestContext(request, context_dict))
