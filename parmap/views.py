ff1ac837a19d41551b3083b5f4c4faaa56f6a7d9
lionellbriones <llenoil@gmail.com>
2017-12-10 17:22:57 +0800

    Added RS layer list

--- parmap/views.py
+++ parmap/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import render_to_response
 
 # Create your views here.
 def other_rs(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render_to_response('parmap/other_rs.html', RequestContext(request, {}))
