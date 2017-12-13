7ef218340b7eda3ef7f4e9180bbc86dadcabc7ce
lionellbriones <llenoil@gmail.com>
2017-12-13 21:58:05 +0800

    Test RS Links

--- parmap/urls.py
+++ parmap/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('parmap.views',
                         url(r'^other_rs/(?P<facettype>[^/]*)$', 'other_rs', name='other_rs'),
+                        url(r'^other_rs/links/(?P<facettype>[^/]*)/(?P<layername>[^/]*)$', 'rs_links', name='rs_links'),
                        )
 
 
--- parmap/views.py
+++ parmap/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+import json
 from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.shortcuts import render_to_response
@@ -9,3 +11,21 @@ def other_rs(request, facettype='layers'):
     }
     
     return render_to_response('parmap/other_rs.html', RequestContext(request, context_dict))
+
+def rs_links(request, facettype='layers', layer_name):
+    context_dict = {
+        "facettype": facettype,
+        "layer_name": layer_name
+        "links": [
+            {
+                "name": 'Sample 1',
+                "url": 'Sample 1',
+            },
+            {
+                "name": 'Sample 2',
+                "url": 'Sample 2',
+            }
+        ]
+    }
+    
+    return HttpResponse(json.dumps(context_dict),mimetype='application/json',status=200)
