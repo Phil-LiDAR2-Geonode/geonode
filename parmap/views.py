6c2693ece6c8063d0d81367cdc203ce4df19766f
Lionell Briones <llenoil@gmail.com>
2017-12-21 07:57:05 +0800

    Test relation

--- parmap/views.py
+++ parmap/views.py
@@ -41,6 +41,7 @@ def _resolve_layer(request, typename, permission='base.view_resourcebase',
 # Create your views here.
 def other_rs(request, facettype='layers'):
     queryset = Layer.objects.distinct().order_by('-date')
+    queryset.owner.all()
 
     context_dict = {
         "list": queryset,
