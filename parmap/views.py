aa26040fa4edade6238c301a75fb5922e0c981cc
Lionell Briones <llenoil@gmail.com>
2017-12-21 09:49:28 +0800

    Updated sample list

--- parmap/views.py
+++ parmap/views.py
@@ -40,7 +40,7 @@ def _resolve_layer(request, typename, permission='base.view_resourcebase',
 
 # Create your views here.
 def other_rs(request, facettype='layers'):
-    queryset = Layer.objects.distinct().filter(title__icontains='3618').order_by('-date')[:5]
+    queryset = Layer.objects.distinct().filter(title__icontains='3618-II').order_by('-date')[:5]
     
     context_dict = {
         "list": queryset,
