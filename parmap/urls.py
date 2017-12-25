cd51d3fb1031341991c09a6a09b4ffc2528de07e
lionellbriones <llenoil@gmail.com>
2017-12-25 20:30:42 +0800

    Removed document links url

--- parmap/urls.py
+++ parmap/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('parmap.views',
                         url(r'^other_rs/(?P<facettype>[^/]*)$', 'other_rs', name='other_rs'),
-                        url(r'^other_rs/links/layers/(?P<layername>[^/]*)$', 'rs_links_layers', name='rs_links_layers'),
-                        url(r'^other_rs/links/maps/(?P<mapname>[^/]*)$', 'rs_links_maps', name='rs_links_maps'),
+                        url(r'^other_rs/links/layers/(?P<layername>[^/]*)$', 'rs_links_layers', name='rs_links_layers')
                        )
 
 
