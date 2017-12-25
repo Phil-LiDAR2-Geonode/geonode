395d8c59067498313a0abb00ca714af251f9000d
Parmap <you@example.com>
2017-12-25 15:24:09 +0800

    merged bug fixed from parmap-prod

--- spectral_library/urls.py
+++ spectral_library/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('spectral_library.views',
                         url(r'^$', 'index', name='index'),
-                        url(r'^browse_queue/(?P<target_type>[\w\s]+)/(?P<prov>[\w\s]+)/$', 'browse_queue', name='browse_queue'),
+                        url(r'^browse_queue/$', 'browse_queue', name='browse_queue'),
                         url(r'^browse_queue/(?P<targets>[\w\s]+)/$', 'download_files', name='download_files'),
                         url(r'^filter_by_target/(?P<target_type>[\w\s]+)/(?P<prov>[\w\s]+)/$', 'filter_by_target', name='filter_by_target'),
                         url(r'^target/(?P<target_id>\d+)/?$', 'target_detail', name='target_detail'),
