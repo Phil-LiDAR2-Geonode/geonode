417c16c939150109b78d241d8f9339b4dbb0176f
lionellbriones <llenoil@gmail.com>
2017-12-28 19:04:21 +0800

    Update rs maps filter

--- parmap/views.py
+++ parmap/views.py
@@ -48,7 +48,7 @@ def other_rs(request, facettype='layers'):
     if(facettype == 'layers'):
         queryset = Layer.objects.distinct().exclude(typename__icontains='_lulc').exclude(typename__icontains='_va').order_by('-date')
     else:
-        queryset = Document.objects.distinct().exclude(doc_file__icontains='_lulc').exclude(doc_file__icontains='_va').order_by('-date')
+        queryset = Document.objects.distinct().exclude(doc_file__icontains='_lulc').exclude(doc_file__icontains='_va').exclude(doc_file__icontains='LANDCOVER').order_by('-date')
 
 
     context_dict = {
