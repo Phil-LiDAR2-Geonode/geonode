d716a026c8b8fdcfa84725bfe55e0cd4200caca3
Lionell Briones <llenoil@gmail.com>
2018-01-04 07:05:36 +0800

    Updated other rs sql query

--- parmap/views.py
+++ parmap/views.py
@@ -47,11 +47,11 @@ def _resolve_layer(request, typename, permission='base.view_resourcebase',
 # Create your views here.
 def other_rs(request, facettype='layers'):
     if(facettype == 'layers'):
-        # queryset = Layer.objects.distinct().exclude(typename__icontains='_lulc').exclude(typename__icontains='_va').exclude(title__icontains='Metadata').order_by('-date')
-        queryset = Layer.objects.distinct().exclude(typename__icontains='_lulc').exclude(typename__icontains='_va').order_by('-date')
+        queryset = Layer.objects.distinct().exclude(typename__icontains='_lulc').exclude(typename__icontains='_va').exclude(title__icontains='Metadata').order_by('-date')
+        # queryset = Layer.objects.distinct().exclude(typename__icontains='_lulc').exclude(typename__icontains='_va').order_by('-date')
     else:
-        # queryset = Document.objects.distinct().exclude(doc_file__icontains='_lulc').exclude(doc_file__icontains='_va').exclude(doc_file__icontains='LANDCOVER').order_by('-date')
-        queryset = Document.objects.distinct().order_by('-date')[:5]
+        queryset = Document.objects.distinct().exclude(doc_file__icontains='_lulc').exclude(doc_file__icontains='_va').exclude(doc_file__icontains='LANDCOVER').order_by('-date')
+        # queryset = Document.objects.distinct().order_by('-date')[:5]
 
 
     context_dict = {
