f3d53106600c52560772c7ffea7eee0a4cfbf5f3
Jok Laurente <jmelaurente@gmail.com>
2018-01-05 02:39:58 +0800

    added urls for pdf view

--- parmap_docs/urls.py
+++ parmap_docs/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('parmap_docs.views',
-                        url(r'^publication/$', 'browse_publication', name='browse_publication'),
-                        # url(r'^publication/(?P<publication_id>\d+)/?$', 'publication_detail', name='publication_detail'),
+                        url(r'^publication/$', 'publication_list', name='publication_list'),
+                        url(r'^publication/(?P<publication_id>\d+)/?$', 'publication_view', name='publication_view'),
                         # url(r'^publication/(?P<publication_id>\d+)/download/$', 'download', name='download'),
                         # url(r'^techreport/(?P<techreport_type>[\w\s]+)/$', 'browse_techreport', name='browse_techreport'),
                         # url(r'^techreport/(?P<techreport_id>\d+)/?$', 'techreport_detail', name='techreport_detail'),
