e97f8d0280028a4b4b2dcb12dad24106d55111a0
Lionell Briones <llenoil@gmail.com>
2018-01-07 06:29:16 +0800

    added template

--- parmap_data_request/views.py
+++ parmap_data_request/views.py
@@ -140,7 +140,7 @@ def test_related(request):
     layer_title = unicode(title_search).encode('utf8')
     layer_resource = get_object_or_404(Layer, title=layer_title)
 
-    return render_to_response('parmap_data_request/target_filter_results.html',RequestContext(request, {
+    return render_to_response('parmap_data_request/test_related.html',RequestContext(request, {
         'title': title_search,
         'resource': layer_resource
     }))
