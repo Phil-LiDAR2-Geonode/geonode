080b4bfe42d6c41c1b5d7c336baab60b0af10afd
Lionell Briones <llenoil@gmail.com>
2018-01-10 01:37:36 +0800

    Update indention

--- parmap_data_request/admin.py
+++ parmap_data_request/admin.py
@@ -69,28 +69,28 @@ class DataRequestAdmin(admin.ModelAdmin):
                     resource_keywords = layer_resource.keywords.names()
                     email_template = 'parmap_data_request/email_approval_layer_lulc.html'
 
-                if "_va" in layer_resource.typename:
-                    typename = '_'.join(layer_resource.typename.split(":")[1].split("_")[:2])
+                    if "_va" in layer_resource.typename:
+                        typename = '_'.join(layer_resource.typename.split(":")[1].split("_")[:2])
 
-                    for related_layer in Layer.objects.filter(typename__icontains=typename):
-                        resources.append(related_layer)
+                        for related_layer in Layer.objects.filter(typename__icontains=typename):
+                            resources.append(related_layer)
 
-                    if 'national' in layer_resource.typename:
-                        html_content = render_to_string('parmap_data_request/email_approval_layer_va_national.html', context)
+                        if 'national' in layer_resource.typename:
+                            html_content = render_to_string('parmap_data_request/email_approval_layer_va_national.html', context)
+                        else:
+                            html_content = render_to_string('parmap_data_request/email_approval_layer_va_local.html', context)
+                                
                     else:
-                        html_content = render_to_string('parmap_data_request/email_approval_layer_va_local.html', context)
-                        
-                else:
-                    muncode_file = staticfiles_storage.path('geonode/files/NSO_Muni.csv')
-                    with open(muncode_file, 'rb') as csvfile:
-                        csv_reader = csv.reader(csvfile)
-                        for row in csv_reader:
-                            csv_contents.append(row[3])
+                        muncode_file = staticfiles_storage.path('geonode/files/NSO_Muni.csv')
+                        with open(muncode_file, 'rb') as csvfile:
+                            csv_reader = csv.reader(csvfile)
+                            for row in csv_reader:
+                                csv_contents.append(row[3])
 
-                    for keyword in resource_keywords:
-                        if keyword in csv_contents:
-                            for related_layer in Layer.objects.filter(keywords__name__in=[keyword]):
-                                resources.append(related_layer)
+                        for keyword in resource_keywords:
+                            if keyword in csv_contents:
+                                for related_layer in Layer.objects.filter(keywords__name__in=[keyword]):
+                                    resources.append(related_layer)
 
                     # remove duplicate items
                     resources = set(resources)
