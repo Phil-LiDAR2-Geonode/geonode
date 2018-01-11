3c3e4dc8d5fb7a5be1760569bde1f6e8994be2a5
Jok Laurente <jmelaurente@gmail.com>
2018-01-11 22:11:55 +0800

    added error message for unauthenticated users

--- spectral_library/views.py
+++ spectral_library/views.py
@@ -13,6 +13,7 @@ from django.utils import timezone
 import os
 import zipfile
 import StringIO
+from django.utils.translation import ugettext as _
 
 
 def index(request):
@@ -101,22 +102,54 @@ def browse_queue(request):
         }))
 
 def download_files(request, targets):
-    target_list = targets.split()
+    try:
+        target_list = targets.split()
 
-    date = timezone.now()
-    zip_subdir = "_".join(["spectro", request.user.username, date.strftime('%Y%m%d'), date.strftime('%H%M%S')])
-    zip_filename = "%s.zip" % zip_subdir
+        date = timezone.now()
+        zip_subdir = "_".join(["spectro", request.user.username, date.strftime('%Y%m%d'), date.strftime('%H%M%S')])
+        zip_filename = "%s.zip" % zip_subdir
 
-    s = StringIO.StringIO()
+        s = StringIO.StringIO()
 
-    zf = zipfile.ZipFile(s, "w")
+        zf = zipfile.ZipFile(s, "w")
 
-    for target_id in target_list:
-        target_path = os.path.join(settings.MEDIA_ROOT, str(Target.objects.get(pk=target_id).target_file))
-        fdir, fname = os.path.split(target_path)
-        zip_path = os.path.join(zip_subdir, fname)
+        for target_id in target_list:
+            target_path = os.path.join(settings.MEDIA_ROOT, str(Target.objects.get(pk=target_id).target_file))
+            fdir, fname = os.path.split(target_path)
+            zip_path = os.path.join(zip_subdir, fname)
 
-        zf.write(target_path, zip_path)
+            zf.write(target_path, zip_path)
+            # tracking of downloads
+            dd = DataDownload()
+            dd.username = Profile.objects.get(username=request.user.username)
+            dd.email = Profile.objects.get(username=request.user.username).email
+            dd.first_name = Profile.objects.get(username=request.user.username).first_name
+            dd.last_name = Profile.objects.get(username=request.user.username).last_name
+            dd.data_id = target_id
+            dd.data_downloaded = Target.objects.get(pk=target_id)
+            dd.data_type = "Spectral Signature"
+            dd.date_downloaded = timezone.now()
+            dd.save()
+
+        zf.close()
+
+        resp = HttpResponse(s.getvalue(), mimetype = "application/x-zip-compressed")
+        resp['Content-Disposition'] = 'attachment; filename=%s' % zip_filename
+
+        user = Profile.objects.get(username=request.user.username)
+        q = Queue.objects.filter(profile_id=user.id)
+        q.delete()
+
+        return resp
+    except:
+        return HttpResponse(
+            loader.render_to_string(
+                '401.html', RequestContext(
+                    request, {
+                        'error_message': _("You are not allowed to download this data.")})), status=403)
+
+def download(request, target_id):
+    try:
         # tracking of downloads
         dd = DataDownload()
         dd.username = Profile.objects.get(username=request.user.username)
@@ -128,37 +161,18 @@ def download_files(request, targets):
         dd.data_type = "Spectral Signature"
         dd.date_downloaded = timezone.now()
         dd.save()
-
-    zf.close()
-
-    resp = HttpResponse(s.getvalue(), mimetype = "application/x-zip-compressed")
-    resp['Content-Disposition'] = 'attachment; filename=%s' % zip_filename
-
-    user = Profile.objects.get(username=request.user.username)
-    q = Queue.objects.filter(profile_id=user.id)
-    q.delete()
-
-    return resp
-
-def download(request, target_id):
-    # tracking of downloads
-    dd = DataDownload()
-    dd.username = Profile.objects.get(username=request.user.username)
-    dd.email = Profile.objects.get(username=request.user.username).email
-    dd.first_name = Profile.objects.get(username=request.user.username).first_name
-    dd.last_name = Profile.objects.get(username=request.user.username).last_name
-    dd.data_id = target_id
-    dd.data_downloaded = Target.objects.get(pk=target_id)
-    dd.data_type = "Spectral Signature"
-    dd.date_downloaded = timezone.now()
-    dd.save()
-    # download zipped file
-    target_path = os.path.join(settings.MEDIA_ROOT, str(Target.objects.get(pk=target_id).target_file))
-    target_file = os.path.basename(target_path)
-    fp = open(target_path, 'rb')
-    response = HttpResponse(fp.read())
-    fp.close()
-    response['content_type'] = "application/x-zip-compressed"
-    response['Content-Disposition'] = 'attachment;filename=%s ' % target_file
-
-    return response
+        # download zipped file
+        target_path = os.path.join(settings.MEDIA_ROOT, str(Target.objects.get(pk=target_id).target_file))
+        target_file = os.path.basename(target_path)
+        fp = open(target_path, 'rb')
+        response = HttpResponse(fp.read())
+        fp.close()
+        response['content_type'] = "application/x-zip-compressed"
+        response['Content-Disposition'] = 'attachment;filename=%s ' % target_file
+        return response
+    except:
+        return HttpResponse(
+            loader.render_to_string(
+                '401.html', RequestContext(
+                    request, {
+                        'error_message': _("You are not allowed to download this data.")})), status=403)
--- uas/views.py
+++ uas/views.py
@@ -10,6 +10,8 @@ from django.shortcuts import render
 from django.conf import settings
 from django.utils import timezone
 import os
+from django.utils.translation import ugettext as _
+
 
 def index(request):
     return render(request, 'uas/uas_index.html')
@@ -39,23 +41,30 @@ def imagery_detail(request, imagery_id):
         }))
 
 def download(request, imagery_id):
-    # tracking of downloads
-    dd = DataDownload()
-    dd.username = Profile.objects.get(username=request.user.username)
-    dd.email = Profile.objects.get(username=request.user.username).email
-    dd.first_name = Profile.objects.get(username=request.user.username).first_name
-    dd.last_name = Profile.objects.get(username=request.user.username).last_name
-    dd.data_id = imagery_id
-    dd.data_downloaded = Imagery.objects.get(pk=imagery_id)
-    dd.data_type = "UAS Imagery"
-    dd.date_downloaded = timezone.now()
-    dd.save()
-    # download zipped file
-    imagery_path = os.path.join(settings.MEDIA_ROOT, str(Imagery.objects.get(pk=imagery_id).zipped_file))
-    imagery_file = os.path.basename(imagery_path)
-    fp = open(imagery_path, 'rb')
-    response = HttpResponse(fp.read())
-    fp.close()
-    response['content_type'] = "application/x-zip-compressed"
-    response['Content-Disposition'] = 'attachment;filename=%s ' % imagery_file
-    return response
+    try:
+        # tracking of downloads
+        dd = DataDownload()
+        dd.username = Profile.objects.get(username=request.user.username)
+        dd.email = Profile.objects.get(username=request.user.username).email
+        dd.first_name = Profile.objects.get(username=request.user.username).first_name
+        dd.last_name = Profile.objects.get(username=request.user.username).last_name
+        dd.data_id = imagery_id
+        dd.data_downloaded = Imagery.objects.get(pk=imagery_id)
+        dd.data_type = "UAS Imagery"
+        dd.date_downloaded = timezone.now()
+        dd.save()
+        # download zipped file
+        imagery_path = os.path.join(settings.MEDIA_ROOT, str(Imagery.objects.get(pk=imagery_id).zipped_file))
+        imagery_file = os.path.basename(imagery_path)
+        fp = open(imagery_path, 'rb')
+        response = HttpResponse(fp.read())
+        fp.close()
+        response['content_type'] = "application/x-zip-compressed"
+        response['Content-Disposition'] = 'attachment;filename=%s ' % imagery_file
+        return response
+    except:
+        return HttpResponse(
+            loader.render_to_string(
+                '401.html', RequestContext(
+                    request, {
+                        'error_message': _("You are not allowed to download this data.")})), status=403)
